3a6b386f9b9adbfb602efa967d5152949865cff8
Winston Win <twin3317@uni.sydney.edu.au>
2025-08-26 18:11:49 +1000

    testing

--- verifier.py
+++ verifier.py
@@ -26,7 +26,8 @@ def main(args: list[str]) -> None:
         try:
             master_file=Path(argv[1])
             single_files=Path(argv[2])
-            if "/" not in master_file:
+
+            if "/" not in master_file.parts:
                  print("invalid arguments")
                  return
             master_lines=master_file.read_text().split("\n")
