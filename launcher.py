f1328b249039ded1cf5d6e4ec0a8632284e48bf8
Thet Naing Win @ Winston <twin3317@uni.sydney.edu.au>
2025-08-26 18:11:39 +1000

    testing launcher

--- launcher.py
+++ launcher.py
@@ -3,11 +3,30 @@ Write code for your launcher here.
 
 You may import library modules allowed by the specs, as well as your own other modules.
 """
+import os
+import sys
+
 from sys import argv
+
+def validation(master_configuration):
+    try:
+        root_port=int(master_configuration[0])
+    except ValueError:
+        return False
 
 
 def main(args: list[str]) -> None:
-    # TODO
+    if len(sys.argv) !=3:
+        print('Please input the master configuration file and directory of single file')
+        sys.exit(1)
+    
+    master_confi=sys.argv[1]
+    singlefile_output=sys.argv[2]
+
+    if not os.path.isfile(master_confi):
+        print("INVALID MASTER\n")
+        sys.exit(1)
+    
     pass
 
 
