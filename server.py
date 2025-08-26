2865d2e234a83a073bc9c614bbca90d19275762f
Thet Naing Win @ Winston <twin3317@uni.sydney.edu.au>
2025-08-26 18:11:40 +1000

    server.py

--- server.py
+++ server.py
@@ -32,7 +32,7 @@ def root_responses(domain,port,config):
     if target_port is not None:
         return str(target_port)
     else:
-        return "NXDOMAIN"
+        return "Hi"
     
 def get_port(domain,config):
     for line in config[1:]:
