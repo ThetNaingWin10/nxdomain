2b88013a35fa0603857147ade09b6550599ea222
Winston Win <twin3317@uni.sydney.edu.au>
2025-08-26 18:11:44 +1000

    NXDOMAIN

--- recursor.py
+++ recursor.py
@@ -37,37 +37,39 @@ def resolve_domain(server_socket,time_out,domain):
         data=(server_socket.recv(1024).decode('utf-8')) #received the TLD port
         if(data):
             tld_port=int(data)
-        else:
-            print("No data received")
-        tld_server_ip, tld_server_port = server_socket.getpeername()
-        #Query the TLD
-        tld_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        tld_socket.connect((tld_server_ip,tld_port))
-        tld_socket.send(f"{domain}\n".encode("utf-8"))
+            tld_server_ip, tld_server_port = server_socket.getpeername()
+            tld_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            tld_socket.connect((tld_server_ip,tld_port))
+            tld_socket.send(f"{domain}\n".encode("utf-8"))
 
-        #port of the authoritative nameserver
-        #testing
-        response=tld_socket.recv(1024).decode("utf-8")
+            #port of the authoritative nameserver
+            #testing
+            response=tld_socket.recv(1024).decode("utf-8")
 
-        if(response.startswith("NXDOMAIN")):
-            print("NXDOMAIN",flush=True)
-        else:
-            auth_port=response
-            auth_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            auth_socket.connect((server_socket.getpeername()[0],int(auth_port)))
-            auth_socket.send(f"{domain}\n".encode("utf-8"))
-
-            ip=auth_socket.recv(1024).decode("utf-8")
-
-
-            timetaken=time.time()-starttime
-
-            if(timetaken>time_out):
+            if(response.startswith("NXDOMAIN")):
                 print("NXDOMAIN",flush=True)
             else:
-                print(f"{ip}",flush=True)
-            
-            timetaken=time.time()-starttime
+                auth_port=response
+                auth_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+                auth_socket.connect((server_socket.getpeername()[0],int(auth_port)))
+                auth_socket.send(f"{domain}\n".encode("utf-8"))
+
+                ip=auth_socket.recv(1024).decode("utf-8")
+
+
+                timetaken=time.time()-starttime
+
+                if(timetaken>time_out):
+                    print("NXDOMAIN",flush=True)
+                else:
+                    print(f"{ip}",flush=True)
+                
+                timetaken=time.time()-starttime
+        else:
+            print("No data received")
+        
+        #Query the TLD
+        
 
 def main(args: list[str]) -> None:
     if len(sys.argv) !=3:
