da81169512a5743f14dafd5515572505d0b51272
Winston Win <twin3317@uni.sydney.edu.au>
2025-08-26 18:11:46 +1000

    verifier testing

--- verifier.py
+++ verifier.py
@@ -37,7 +37,7 @@ def main(args: list[str]) -> None:
                         domain_check=parts[0]
                         porting_address=parts[1]
 
-            if domain_check==domain.split(".")[-1]:
+            if domain_check==domain.rsplit(".",3):
                 print(porting_address)
                 currentport=porting_address
 
