dbfba2514ad829c36c7216da4b3ea452d0554671
Thet Naing Win @ Winston <twin3317@uni.sydney.edu.au>
2025-08-26 18:11:40 +1000

    server implementation

--- recursor.py
+++ recursor.py
@@ -3,11 +3,24 @@ Write code for your recursor here.
 
 You may import library modules allowed by the specs, as well as your own other modules.
 """
+import socket
+import sys
+import time
+
 from sys import argv
 
 
 def main(args: list[str]) -> None:
-    # TODO
+    if len(sys.argv) !=3:
+        print("INVALID ARGUMENTS")
+        sys.exit(1)
+    try:
+        root=int(sys.argv[1])
+        timeout=float(sys.argv[2])
+
+    except Exception as e:
+        print("Error",str(e))
+        
     pass
 
 
