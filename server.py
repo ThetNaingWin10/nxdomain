71635af76a0e00624ee5499bf64e4c08fa07cb70
Winston Win <twin3317@uni.sydney.edu.au>
2025-08-26 18:11:42 +1000

    server.py

--- server.py
+++ server.py
@@ -85,6 +85,7 @@ def main(args: list[str]) -> None:
                     else:
                         response=root_responses(data,port,config)
                         socket_client.send((response+'\n').encode("utf-8"))
+                        dns_records[f"{data}"]=response
                         print(f"resolve {data} to {response}",flush=True)
                         # if response!="NXDOMAIN":
                         #     dns_records[f"{data}"]=response
