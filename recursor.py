7b03fcdff5afff45e553dd83af5b6aaa7731ec8b
Winston Win <twin3317@uni.sydney.edu.au>
2025-08-26 18:11:47 +1000

    testing

--- recursor.py
+++ recursor.py
@@ -38,7 +38,7 @@ def valid(domain_name):
 
 def resolve_domain(root_serversocket,time_out,domain):
         signal.signal(signal.SIGALRM,timeoutsignal)
-        signal.alarm(round(int(time_out)))
+        signal.alarm(round(time_out))
         try:
 
             root_serversocket.send(f"{domain.split('.')[-1]}\n".encode('utf-8'))
