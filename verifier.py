58a4cfd708f7d9594d619fcf90fcf72d6033e113
Winston Win <twin3317@uni.sydney.edu.au>
2025-08-26 18:11:52 +1000

    Testing

--- verifier.py
+++ verifier.py
@@ -11,12 +11,19 @@ master_list=[]
 
 def check(list,contents):
      i=0
+     portstogo=[]
      for line in contents:
         if "," in line:
              line=line.split(",")
              for check_domain in list:
                   if check_domain==line[0]:
-                       print(line[1])
+                       portstogo.append(line[1]) ## validating each domains
+     if len(portstogo)!=len(contents):
+          return "neq"
+     else:
+          return portstogo
+     
+     
                        
              
     
