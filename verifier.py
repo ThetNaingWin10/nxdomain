b1540d97fee5b2ad770d3c6e3514448e0f062ac9
Winston Win <twin3317@uni.sydney.edu.au>
2025-08-26 18:11:53 +1000

    Testing

--- verifier.py
+++ verifier.py
@@ -84,8 +84,6 @@ def main(args: list[str]) -> None:
                 list2.append(middomain)
                 list3.append(line[0])     
 
-        for port in currentport:
-             print(port)
         for items in single_files.iterdir():
              if items.is_file():
                   with items.open("r") as line:
