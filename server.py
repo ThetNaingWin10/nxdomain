f401cb5003d4ab80604952c35b99a29f3098b1db
Thet Naing Win @ Winston <twin3317@uni.sydney.edu.au>
2025-08-26 18:11:41 +1000

    server.py

--- server.py
+++ server.py
@@ -62,10 +62,9 @@ def main(args: list[str]) -> None:
                 while True:
                     socket_client , _ = server_socket.accept()
                     domain=socket_client.recv(server_port).decode("utf-8").strip()
-                    response=root_responses(domain,port,config)
-                    if(response=="NXDOMAIN"):
-                        break
+                    response=root_responses(domain,port,config) 
                     socket_client.send((response+'\n').encode("utf-8"))
+                    print(response)
                     
             #with open(config_file,"r") as file:
                 #for line in file:
