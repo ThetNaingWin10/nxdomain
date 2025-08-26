edc3d19489984f4b0d8893d917d8829e5d61387a
Thet Naing Win @ Winston <twin3317@uni.sydney.edu.au>
2025-08-26 18:11:41 +1000

    server.py

--- server.py
+++ server.py
@@ -62,7 +62,7 @@ def main(args: list[str]) -> None:
                 while True:
                     socket_client , _ = server_socket.accept()
                     domain=socket_client.recv(server_port).decode("utf-8").strip()
-                    response=root_responses(domain,port,config[1:])
+                    response=root_responses(domain,port,config)
                     if(response==None):
                         break
                     socket_client.send((response+'\n').encode("utf-8"))
