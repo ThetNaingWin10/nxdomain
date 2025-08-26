2834cce9120055cebbd82ab1982c75bcb8b05ab1
Winston Win <twin3317@uni.sydney.edu.au>
2025-08-26 18:11:49 +1000

    testing

--- verifier.py
+++ verifier.py
@@ -55,13 +55,12 @@ def main(args: list[str]) -> None:
         if(target_port<int(currentport)):
              print("invalid master") ## validating the current port and targetport 
              return
+        print(currentport)
+        print(target_port)
         if(currentport==1024):  ## specifically for the google testcase
              if(target_port==1029) :
                   print("invalid master")
                   return
-        print(currentport)
-        print(domain)
-        print(target_port)
 
         domain_length=domain.split(".")
         i=len(domain_length)-1
