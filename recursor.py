a230cf18a8798863732ba6920e5cc97bdeaffcd6
Thet Naing Win @ Winston <twin3317@uni.sydney.edu.au>
2025-08-26 18:11:40 +1000

    recursor implementation

--- recursor.py
+++ recursor.py
@@ -9,6 +9,24 @@ import time
 
 from sys import argv
 
+def resolve_domain(root_port, timeout) :
+    try:
+        root_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM) #socket to connect to the root directory
+        root_socket.connect(("localhost",root_port))
+        while True:
+            domain=input("Please Enter a Domian or CtrlD to exit") 
+            if not domain:
+                break
+        root_socket.send(f"{domain}\n".encode("utf-8")) #send the query to the domain server
+        
+        startingtime=time.time() #starting the timer
+
+        tld_port=root_socket.recv(1024).decode("utf-8").strip() # receiving the response from the root server
+
+        duration= time.time()-startingtime # checking if the duration of the program running exceeds the timeout
+        if(duration>timeout) :
+            print("NXDOMAIN (Timeout)")
+
 
 def main(args: list[str]) -> None:
     if len(sys.argv) !=3:
