1a927a570f0d870c17d3c240705ecc1e16b0f132
Winston Win <twin3317@uni.sydney.edu.au>
2025-08-26 18:11:49 +1000

    verifier

--- verifier.py
+++ verifier.py
@@ -47,12 +47,10 @@ def main(args: list[str]) -> None:
              print("invalid master") ## validaing if there is a full domain.
              return
         
-        if(target_port-int(currentport)<10):
-             print("invalid master")
-             return
-        # print(currentport)
-        # print(domain)
-        # print(target_port)
+        print(currentport)
+        print(domain)
+        print(target_port)
+        
         domain_length=domain.split(".")
         i=len(domain_length)-1
         # z=0
