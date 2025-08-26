00094bf1f3f83d9cc9039f8f5a40bdaaf747642f
Thet Naing Win @ Winston <twin3317@uni.sydney.edu.au>
2025-08-26 18:11:40 +1000

    server.py

--- server.py
+++ server.py
@@ -63,7 +63,7 @@ def main(args: list[str]) -> None:
                     socket_client , _ = server_socket.accept()
                     domain=socket_client.recv(server_port).decode("utf-8").strip()
                     response=root_responses(domain,port,config)
-                    socket_client.send(response.encode("utf-8"))
+                    socket_client.send((response+"\n").encode("utf-8"))
             #with open(config_file,"r") as file:
                 #for line in file:
                   #  socket_client.send(line.encode("utf-8"))
