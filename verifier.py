1f7c11505e85eafbb1d1dbd34c52eb26c396328f
Winston Win <twin3317@uni.sydney.edu.au>
2025-08-26 18:11:49 +1000

    testing

--- verifier.py
+++ verifier.py
@@ -26,6 +26,9 @@ def main(args: list[str]) -> None:
         try:
             master_file=Path(argv[1])
             single_files=Path(argv[2])
+            if "/" not in master_file:
+                 print("invalid arguments")
+                 return
             master_lines=master_file.read_text().split("\n")
         # except FileNotFoundError:
         #      print("invalid arguments")
